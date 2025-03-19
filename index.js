ee5814e0b2200db952114bdc097b96d98641b347
DirsanA <dirsanantehun739@gmail.com>
2025-03-18 23:05:37 -0700

    Add controller folder is added for making code modularize

--- index.js
+++ index.js
@@ -1,62 +1,48 @@
 const express = require("express");
-
 const app = express();
 
+app.use(express.json()); // Middleware to parse JSON requests
+
 const users = [
-  {
-    id: 0,
-    name: "Samuel Antehun",
-  },
-  {
-    id: 1,
-    name: "Dirsan Antehun",
-  },
-  {
-    id: 2,
-    name: "Hani Antehun",
-  },
+  { id: 0, name: "Samuel Antehun" },
+  { id: 1, name: "Dirsan Antehun" },
+  { id: 2, name: "Hani Antehun" },
 ];
 
-// getting all user
-
+// Getting all users
 app.get("/users", function (req, res) {
   res.status(200).send({
-    message: "sucess",
+    message: "Success",
     data: users,
   });
 });
 
-// getting user by its id
-
+// Getting a user by ID
 app.get("/users/:id", function (req, res) {
-  const id = req.params.id;
-  const user = users[id];
+  const id = parseInt(req.params.id); // Ensure id is an integer
+  const user = users.find((u) => u.id === id);
 
   if (!user) {
-    return res.status(404).send("EROR: User not found ");
+    return res.status(404).send({ message: "ERROR: User not found" });
   }
   return res.status(200).send(user);
 });
-// adding user by post
 
-app.use(express.json());
-app.get("/users/add", function (req, res) {
+// Adding a user (should be a POST request)
+app.post("/users/add", function (req, res) {
   const newUserName = req.body.name;
 
   if (!newUserName) {
-    return res.status(400).send({
-      message: "Error :User Name is required",
-    });
-    const newUser = {
-      name: newUserName,
-      id: users.length,
-    };
-    users.push(newUser);
-    return res.status(200).send({
-      message: "Added sucessfuly",
-      data: newUser,
-    });
+    return res.status(400).send({ message: "Error: User Name is required" });
   }
+
+  const newUser = {
+    id: users.length, // Assigning a new ID
+    name: newUserName,
+  };
+
+  users.push(newUser);
+  return res.status(201).send({ message: "Added successfully", data: newUser });
 });
 
 const PORT = 5000;
